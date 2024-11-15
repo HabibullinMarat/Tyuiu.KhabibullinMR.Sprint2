7666306f550053e58412306ee8bb93e717d1f5f2
habibullinm52@gmail.com <habibullinm52@gmail.com>
2024-11-15 22:13:14 +0500

    Task6

--- Tyuiu.KhabibullinMR.Sprint2.Task6.V9.Lib/DataService.cs
+++ Tyuiu.KhabibullinMR.Sprint2.Task6.V9.Lib/DataService.cs
@@ -8,8 +8,12 @@ namespace Tyuiu.KhabibullinMR.Sprint2.Task6.V9.Lib
         {
             switch (m, n)
             {
-                case (< 13,<31):return $"{m} месяц,{n + 1} число";
-                case ( < 12, 31):return $"{m+1} месяц,1 число";
+                case ( < 10, < 9):return $"0{n + 1}.0{m}";
+                case ( < 13, < 9): return $"0{n + 1}.{m}";
+                case ( < 10, < 31): return $"{n + 1}.0{m}";
+                case ( < 13, < 31): return $"{n + 1}.{m}";
+                case ( < 9, 31): return $"01.0{m+1}";
+                case ( < 12, 31): return $"01.{m+1}";
                 default: throw new ArgumentException("Такого дня в году нет/m и n не характеризуют 31 декабря");
             }
         }
