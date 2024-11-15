bebbb796bf42f8775bda9f5a8c3ecd58e4111564
habibullinm52@gmail.com <habibullinm52@gmail.com>
2024-11-15 20:59:27 +0500

    Task5

---
+++ Tyuiu.KhabibullinMR.Sprint2.Task5.V5/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.KhabibullinMR.Sprint2.Task5.V5.Lib;
+
+namespace Tyuiu.KhabibullinMR.Sprint2.Task5.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер карты: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ:                                                                 ");
+            Console.WriteLine("***************************************************************************");
+            
+            Console.WriteLine(ds.FindCardValue(x));
+        }
+    }
+}
